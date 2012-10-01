4ab7ef3f03f5d0bb37277750b61f48b48756e267
supudo <supudo@gmail.com>
2012-10-01 17:57:40 +0300

    rev

--- Neolog/Views/ShareFacebook.xaml.cs
+++ Neolog/Views/ShareFacebook.xaml.cs
@@ -8,8 +8,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Neolog.Database.Models;
 using Neolog.Utilities;
 using Facebook;
 
@@ -17,17 +19,124 @@ namespace Neolog.Views
 {
     public partial class ShareFacebook : NeologBasePage
     {
+        private Word currentWord;
+        private readonly FacebookClient _fb = new FacebookClient();
+        private const string ExtendedPermissions = "publish_stream";
+        private string _accessToken;
+        private string _userId;
+        private string _lastMessageId;
+
         public ShareFacebook()
         {
             InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string wid = "";
+            if (NavigationContext.QueryString.TryGetValue("wid", out wid))
+                this.currentWord = App.DbViewModel.GetWord(int.Parse(wid));
+        }
+
         private void wbFacebook_Loaded(object sender, RoutedEventArgs e)
         {
+            var loginUrl = GetFacebookLoginUrl(AppSettings.FacebookAppID, ExtendedPermissions);
+            wbFacebook.Navigate(loginUrl);
+        }
+
+        private Uri GetFacebookLoginUrl(string appId, string extendedPermissions)
+        {
+            var parameters = new Dictionary<string, object>();
+            parameters["client_id"] = AppSettings.FacebookAppID;
+            parameters["redirect_uri"] = "https://www.facebook.com/connect/login_success.html";
+            parameters["response_type"] = "token";
+            parameters["display"] = "touch";
+
+            if (!string.IsNullOrEmpty(extendedPermissions))
+                parameters["scope"] = extendedPermissions;
+
+            return _fb.GetLoginUrl(parameters);
         }
 
         private void wbFacebook_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
         {
+            FacebookOAuthResult oauthResult;
+            if (!_fb.TryParseOAuthCallbackUrl(e.Uri, out oauthResult))
+                return;
+
+            if (oauthResult.IsSuccess)
+            {
+                var accessToken = oauthResult.AccessToken;
+                LoginSucceded(accessToken);
+            }
+            else
+                MessageBox.Show(oauthResult.ErrorDescription);
+        }
+
+        private void LoginSucceded(string accessToken)
+        {
+            var fb = new FacebookClient(accessToken);
+
+            fb.GetCompleted += (o, e) =>
+            {
+                if (e.Error != null)
+                {
+                    Dispatcher.BeginInvoke(() => MessageBox.Show(e.Error.Message));
+                    return;
+                }
+
+                var result = (IDictionary<string, object>)e.GetResultData();
+                var id = (string)result["id"];
+                //var url = string.Format("/Pages/FacebookInfoPage.xaml?access_token={0}&id={1}", accessToken, id);
+                //Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri(url, UriKind.Relative)));
+                _accessToken = accessToken;
+                _userId = id;
+                this.postMessage();
+            };
+
+            fb.GetAsync("me?fields=id");
+        }
+
+        private void postMessage()
+        {
+            if (this.currentWord == null)
+            {
+                MessageBox.Show("Enter message.");
+                return;
+            }
+
+            var fb = new FacebookClient(_accessToken);
+
+            fb.PostCompleted += (o, args) =>
+            {
+                if (args.Error != null)
+                {
+                    Dispatcher.BeginInvoke(() => MessageBox.Show(args.Error.Message));
+                    return;
+                }
+
+                var result = (IDictionary<string, object>)args.GetResultData();
+                _lastMessageId = (string)result["id"];
+
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show(AppResources.thankYou);
+                });
+            };
+
+            var parameters = new Dictionary<string, object>();
+            //parameters["message"] = this.currentOffer.Title;
+            parameters["name"] = this.currentWord.WordContent;
+            parameters["caption"] = this.currentWord.Description;
+            parameters["description"] = this.currentWord.Example;
+            parameters["link"] = "http://www.neolog.bg/word/" + this.currentWord.WordId;
+
+            fb.PostAsync("me/feed", parameters);
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                NavigationService.GoBack();
+            });
         }
     }
 }
--- Neolog/Views/ShareTwitter.xaml.cs
+++ Neolog/Views/ShareTwitter.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Microsoft.Phone.Controls;
+using Neolog.Database.Models;
 using Neolog.Utilities;
 using Neolog.Utilities.Twitter;
 using Hammock;
@@ -20,17 +25,178 @@ namespace Neolog.Views
 {
     public partial class ShareTwitter : NeologBasePage
     {
+        string postMessage = "";
+
+        string OAuthTokenKey = string.Empty;
+        string tokenSecret = string.Empty;
+
+        string accessToken = string.Empty;
+        string accessTokenSecret = string.Empty;
+
+        string userID = string.Empty;
+        string userScreenName = string.Empty;
+
         public ShareTwitter()
         {
             InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string wid = "";
+            if (NavigationContext.QueryString.TryGetValue("wid", out wid))
+            {
+                Word w = App.DbViewModel.GetWord(int.Parse(wid));
+
+                this.postMessage = "Neolog.bg - ";
+                this.postMessage += w.WordContent + ": http://www.neolog.bg/word/" + w.WordId;
+                this.postMessage += " #neologbg";
+            }
+        }
+
         private void wbTwitter_Loaded(object sender, RoutedEventArgs e)
         {
+            accessToken = TwitterHelper.GetKeyValue<string>("AccessToken");
+            accessTokenSecret = TwitterHelper.GetKeyValue<string>("AccessTokenSecret");
+            userScreenName = TwitterHelper.GetKeyValue<string>("ScreenName");
+
+            if (string.IsNullOrEmpty(accessToken) || string.IsNullOrEmpty(accessTokenSecret))
+            {
+                var requestTokenQuery = TwitterOAuthHelper.GetRequestTokenQuery();
+                requestTokenQuery.RequestAsync(AppSettings.TwitterRequestTokenUri, null);
+                requestTokenQuery.QueryResponse += new EventHandler<WebQueryResponseEventArgs>(requestTokenQuery_QueryResponse);
+            }
+            else
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    var tweetButton = (Microsoft.Phone.Shell.ApplicationBarIconButton)this.ApplicationBar.Buttons[3];
+                    tweetButton.IsEnabled = true;
+                });
+            }
         }
 
         private void wbTwitter_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
         {
+            this.wbTwitter.Visibility = Visibility.Visible;
+            this.wbTwitter.Navigated -= wbTwitter_Navigated;
+        }
+
+        void requestTokenQuery_QueryResponse(object sender, WebQueryResponseEventArgs e)
+        {
+            try
+            {
+                var parameters = TwitterHelper.GetQueryParameters((new StreamReader(e.Response)).ReadToEnd());
+                OAuthTokenKey = parameters["oauth_token"];
+                tokenSecret = parameters["oauth_token_secret"];
+                var authorizeUrl = AppSettings.TwitterAuthorizeUri + "?oauth_token=" + OAuthTokenKey;
+
+                Dispatcher.BeginInvoke(() =>
+                {
+                    this.wbTwitter.Navigate(new Uri(authorizeUrl));
+                });
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show(ex.Message);
+                });
+            }
+        }
+
+        private void objAuthorizeBrowserControl_Navigating(object sender, NavigatingEventArgs e)
+        {
+            if (e.Uri.ToString().StartsWith(AppSettings.TwitterCallbackUri))
+            {
+                var AuthorizeResult = TwitterHelper.GetQueryParameters(e.Uri.ToString());
+                var VerifyPin = AuthorizeResult["oauth_verifier"];
+                this.wbTwitter.Visibility = Visibility.Collapsed;
+
+                var AccessTokenQuery = TwitterOAuthHelper.GetAccessTokenQuery(OAuthTokenKey, tokenSecret, VerifyPin);
+
+                AccessTokenQuery.QueryResponse += new EventHandler<WebQueryResponseEventArgs>(AccessTokenQuery_QueryResponse);
+                AccessTokenQuery.RequestAsync(AppSettings.TwitterAccessTokenUri, null);
+            }
+        }
+
+        void AccessTokenQuery_QueryResponse(object sender, WebQueryResponseEventArgs e)
+        {
+            try
+            {
+                var parameters = TwitterHelper.GetQueryParameters((new StreamReader(e.Response)).ReadToEnd());
+                accessToken = parameters["oauth_token"];
+                accessTokenSecret = parameters["oauth_token_secret"];
+                userID = parameters["user_id"];
+                userScreenName = parameters["screen_name"];
+
+                TwitterHelper.SetKeyValue<string>("AccessToken", accessToken);
+                TwitterHelper.SetKeyValue<string>("AccessTokenSecret", accessTokenSecret);
+                TwitterHelper.SetKeyValue<string>("ScreenName", userScreenName);
+
+                Dispatcher.BeginInvoke(() =>
+                {
+                    var SignInMenuItem = (Microsoft.Phone.Shell.ApplicationBarMenuItem)this.ApplicationBar.MenuItems[0];
+                    SignInMenuItem.IsEnabled = false;
+
+                    var SignOutMenuItem = (Microsoft.Phone.Shell.ApplicationBarMenuItem)this.ApplicationBar.MenuItems[1];
+                    SignOutMenuItem.IsEnabled = true;
+
+                    var tweetButton = (Microsoft.Phone.Shell.ApplicationBarIconButton)this.ApplicationBar.Buttons[0];
+                    tweetButton.IsEnabled = true;
+                });
+                this.postMessageToTwitter();
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show(ex.Message);
+                });
+            }
+        }
+
+        private void postMessageToTwitter()
+        {
+            var credentials = new OAuthCredentials
+            {
+                Type = OAuthType.ProtectedResource,
+                SignatureMethod = OAuthSignatureMethod.HmacSha1,
+                ParameterHandling = OAuthParameterHandling.HttpAuthorizationHeader,
+                ConsumerKey = AppSettings.TwitterConsumerKey,
+                ConsumerSecret = AppSettings.TwitterConsumerKeySecret,
+                Token = this.accessToken,
+                TokenSecret = this.accessTokenSecret,
+                Version = "1.0"
+            };
+
+            var restClient = new RestClient
+            {
+                Authority = AppSettings.TwitterStatusUpdateUrl,
+                HasElevatedPermissions = true,
+                Credentials = credentials,
+                Method = WebMethod.Post
+            };
+
+            restClient.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            var restRequest = new RestRequest
+            {
+                Path = "1/statuses/update.xml?status=" + this.postMessage
+            };
+
+            var ByteData = Encoding.UTF8.GetBytes(this.postMessage);
+            restRequest.AddPostContent(ByteData);
+            restClient.BeginRequest(restRequest, new RestCallback(postFinished));
+        }
+
+        private void postFinished(RestRequest request, Hammock.RestResponse response, object obj)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                //MessageBox.Show(AppResources.offer_ThankYou);
+                NavigationService.GoBack();
+            });
         }
     }
 }
